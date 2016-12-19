422bd482b582fc8f19ba559e765ea7854e8d959c
kicoamorim@gmail.com <kicoamorim@gmail.com>
2016-12-19 16:45:32 -0200

    add employee

--- src/main/java/com/example/DataLoader.java
+++ src/main/java/com/example/DataLoader.java
@@ -19,6 +19,7 @@ public class DataLoader implements CommandLineRunner{
 		this.repository.save(new Employee("Luis Amorim",62, 10));
 		this.repository.save(new Employee("Adriano",63, 13));
 		this.repository.save(new Employee("Regis",64, 14));
+		this.repository.save(new Employee("Jaco Pastorius",60,15));
 		
 	}
 }
